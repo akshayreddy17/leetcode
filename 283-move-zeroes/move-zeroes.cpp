543f6c6c1cf48ef3cfa243cc37e9307bd2a3592f
akshayreddy17 <22MH1A4953@acoe.edu.in>
2025-02-22 22:13:17 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (18.13%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int lastNonZeroFoundAt = 0;
+        for (int current = 0; current < nums.size(); current++) {
+            if (nums[current] != 0) {
+                swap(nums[lastNonZeroFoundAt++], nums[current]);
+            }
+        }
+        for (int i = lastNonZeroFoundAt; i < nums.size(); ++i) {
+            nums[i] = 0;
+        }
+    }
+};
